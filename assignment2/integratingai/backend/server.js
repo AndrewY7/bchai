da3aa72f419d61c65364a9577b9b16ede38ea124
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2024-09-26 17:17:35 -0400

    added server links

--- assignment2/integratingai/backend/server.js
+++ assignment2/integratingai/backend/server.js
@@ -7,8 +7,22 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+const allowedOrigins = [
+  'https://andrewy7.github.io',
+  'https://andrewy7.github.io/bchai/', 
+];
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, 
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
